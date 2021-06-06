0390884a0e023e4b001d2a6c9e0136214c5ab53e
Sofía Swidarowicz Andrade <sofia.swidarowicz@gmail.com>
2021-06-06 14:12:46 +0200

    Adding a custom text overlay an image

--- SwiftUI/SwiftUICode/ContentView.swift
+++ SwiftUI/SwiftUICode/ContentView.swift
@@ -14,17 +14,29 @@ struct ContentView: View {
             .edgesIgnoringSafeArea(.all)
             //.scaledToFit()
             .aspectRatio(contentMode: .fit)
-            .frame(width: 300)
-            //.clipped()
-            .clipShape(Circle())
-            //.clipShape(Ellipse())
-            //.clipShape(Capsule())
-            .opacity(0.7)
+//            .frame(width: 300)
+            .clipped()
+//            .clipShape(Circle())
+//            //.clipShape(Ellipse())
+//            //.clipShape(Capsule())
+//            .opacity(0.7)
             .overlay(
-            Image(systemName: "heart.fill")
-                .font(.system(size: 50))
-                .foregroundColor(.black)
-                .opacity(0.5)
+//            Image(systemName: "heart.fill")
+//                .font(.system(size: 50))
+//                .foregroundColor(.black)
+//                .opacity(0.5)
+                Text("If you are lucky enough to have lived in Paris as a young man, then wherever you go for the rest of your life it stays with you, for Paris is a moveable feast.\n\n- Ernest Hemingway")
+                           .fontWeight(.heavy)
+                           .font(.system(.headline, design: .rounded))
+                           .foregroundColor(.white)
+                           .padding()
+                           .background(Color.black)
+                           .cornerRadius(20)
+                           .opacity(0.8)
+                           .padding(),
+
+                       alignment: .top
+
             )
     }
 }
