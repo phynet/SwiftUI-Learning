4052f04473b686042a305157834a7c6f18177141
Sofía Swidarowicz Andrade <sofia.swidarowicz@gmail.com>
2021-08-15 01:24:40 +0200

    scrollview added with header

--- SwiftUI/SwiftUICode/ScrollView.swift
+++ SwiftUI/SwiftUICode/ScrollView.swift
@@ -2,60 +2,42 @@
 //  ScrollView.swift
 //  SwiftUICode
 //
-//  Created by Sofia Swidarowicz on 9/8/21.
+//  Created by Sofia Swidarowicz on 15/8/21.
 //
 
 import SwiftUI
 
-struct ScrollView: View {
-    var image: String
-    var category: String
-    var heading: String
-    var author: String
-
+struct ContentScrollView: View {
     var body: some View {
-        VStack {
-            Image(image)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+        ScrollView {
             HStack {
                 VStack(alignment: .leading) {
-                    Text(category)
-                        .font(.headline)
-                        .foregroundColor(.secondary)
-                    Text(heading)
-                        .font(.title)
-                        .fontWeight(.black)
-                        .foregroundColor(.primary)
-                        .lineLimit(3)
-                    Text("Written by \(author)".uppercased())
+                    Text("Monday, Aug 20".uppercased())
                         .font(.caption)
                         .foregroundColor(.secondary)
-                }
+                    Text("Your Reading")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .fontWeight(.black)
+
+                } //VSTACK
                 Spacer()
+            }.padding([.top, .horizontal])
+
+            VStack {
+                CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+                CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+                CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
             }
-            .padding()
+
+
         }
-        .cornerRadius(10)
-        .overlay(
-            RoundedRectangle(cornerRadius: 10)
-            .stroke(Color(.sRGB,
-                          red: 150/255,
-                          green: 150/255,
-                          blue: 150/255,
-                          opacity: 0.1),
-                    lineWidth: 1)
-        )
-        .padding([.top, .horizontal])
+
     }
 }
 
-struct ScrollView_Previews: PreviewProvider {
+struct ContentScrollView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrollView(
-            image: "swiftui-button",
-            category: "SwiftUI",
-            heading: "Drawing a Border with Rounded Corners",
-            author: "Simon Ng")
+        ContentScrollView()
     }
 }
